c4e953a64e6acbf0b1c9d6b316d02cc6ec46730b
宋正朋 <chestnutTang@163.com>
2017-11-25 11:35:39 +0800

    添加列表的fragment

--- app/src/main/java/demo/third/com/exceldemo/activity/MainActivity.java
+++ app/src/main/java/demo/third/com/exceldemo/activity/MainActivity.java
@@ -8,16 +8,19 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import demo.third.com.exceldemo.R;
+import demo.third.com.exceldemo.fragment.ItemFragment;
 import demo.third.com.exceldemo.fragment.TextFragment;
+import demo.third.com.exceldemo.fragment.dummy.DummyContent;
 
 /**
  * @author songzhengpeng
  */
 public class MainActivity extends BaseActivity implements BottomNavigationView
-        .OnNavigationItemSelectedListener {
+        .OnNavigationItemSelectedListener,ItemFragment.OnListFragmentInteractionListener {
 
     @BindView(R.id.message)
     TextView message;
@@ -27,6 +30,7 @@ public class MainActivity extends BaseActivity implements BottomNavigationView
     RelativeLayout container;
 
     TextFragment textFragment;
+    ItemFragment itemFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,33 +57,51 @@ public class MainActivity extends BaseActivity implements BottomNavigationView
             case R.id.navigation_home:
                 message.setText(R.string.title_home);
                 text = "1111";
+                switchFragmentText(text);
                 break;
             case R.id.navigation_dashboard:
                 message.setText(R.string.title_dashboard);
                 text = "22222";
+                switchFragmentText(text);
                 break;
             case R.id.navigation_notifications:
                 message.setText(R.string.title_notifications);
                 text = "33333";
+                switchFragmentText2();
                 break;
             default:
                 break;
         }
-        switchFragmentText(text);
+
         return true;
     }
 
     private void bindFragment() {
         FragmentManager fragmentManager = getSupportFragmentManager();
         textFragment = new TextFragment();
+        itemFragment = new ItemFragment();
 
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.container, textFragment);
         transaction.commit();
+
+        FragmentTransaction transaction2 = fragmentManager.beginTransaction();
+        transaction2.replace(R.id.container, itemFragment);
+        transaction2.commit();
     }
 
     private void switchFragmentText(String text) {
         textFragment.setTextShow(text);
+        Toast.makeText(getApplicationContext(),"哈哈哈",Toast.LENGTH_SHORT).show();
     }
 
+    private void switchFragmentText2() {
+//        textFragment.setTextShow(text);
+        Toast.makeText(getApplicationContext(),"123123",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Toast.makeText(getApplicationContext(),item.content,Toast.LENGTH_SHORT).show();
+    }
 }
