411d976095f5926045020b588e65cfb06f4b1ab4
宋正朋 <ssongzhengpeng@163.com>
2017-12-08 11:21:12 +0800

    home

--- app/src/main/java/demo/third/com/exceldemo/ui/activity/BaseActivity.java
+++ app/src/main/java/demo/third/com/exceldemo/ui/activity/BaseActivity.java
@@ -15,7 +15,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(getLayoutId());
+        setContentView( getLayoutId());
         ButterKnife.bind(this);
     }
 
