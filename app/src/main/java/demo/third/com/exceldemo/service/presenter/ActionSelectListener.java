22045c89763675c0e975c5a2b4682002c1e7dcc2
宋正朋 <chestnutTang@163.com>
2018-03-12 10:42:03 +0800

    webView可以复制和搜索

---
+++ app/src/main/java/demo/third/com/exceldemo/service/presenter/ActionSelectListener.java
@@ -0,0 +1,11 @@
+package demo.third.com.exceldemo.service.presenter;
+
+/**
+ * peterDemoExcels
+ * Created by peter
+ * on 2018.03
+ */
+
+public interface ActionSelectListener {
+    void onClick(String title, String selectText);
+}
--- app/src/main/java/demo/third/com/exceldemo/ui/activity/MyWebViewActivity.java
+++ app/src/main/java/demo/third/com/exceldemo/ui/activity/MyWebViewActivity.java
@@ -1,11 +1,17 @@
 package demo.third.com.exceldemo.ui.activity;
 
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import demo.third.com.exceldemo.R;
+import demo.third.com.exceldemo.service.presenter.ActionSelectListener;
+import demo.third.com.exceldemo.ui.views.CustomActionWebView;
 import demo.third.com.exceldemo.utils.Link;
+import demo.third.com.exceldemo.utils.Tools;
 
 /**
  *
@@ -14,7 +20,9 @@ public class MyWebViewActivity extends WebViewBaseActivity {
 
 
     @BindView(R.id.web_view)
-    WebView webView;
+    CustomActionWebView webView;
+
+    List<String> menuList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +39,37 @@ public class MyWebViewActivity extends WebViewBaseActivity {
     protected void initView() {
         super.initView();
         initWebViewSetting(webView, Link.WEATHER);
+        webView.setActionSelectListener(new ActionSelectListener() {
+            @Override
+            public void onClick(String title, String selectText) {
+                Tools.toast(title + "\n" + selectText);
+            }
+        });
+        menuList.add("111");
+        menuList.add("222");
+        menuList.add("333");
+        webView.setActionList(menuList);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+//        if (webView != null) {
+//            webView.clearAnimation();
+//            webView.clearHistory();
+//            ((ViewGroup) webView.getParent()).removeView(webView);
+//            webView.destroy();
+//            webView = null;
+//        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
