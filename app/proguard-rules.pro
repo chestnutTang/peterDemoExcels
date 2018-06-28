d6c835a74d31a79b0f030acd166775c824e26dae
宋正朋 <chestnutTang@163.com>
2018-06-28 17:00:20 +0800

    添加阿里云热修复

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -227,5 +227,15 @@
 -keepclassmembers class android.support.design.internal.BottomNavigationMenuView {
     boolean mShiftingMode;
 }
+#基线包使用，生成mapping.txt
+-printmapping mapping.txt
+#生成的mapping.txt在app/build/outputs/mapping/release路径下，移动到/app路径下
+#修复后的项目使用，保证混淆结果一致
+#-applymapping mapping.txt
+#hotfix
+-keep class com.taobao.sophix.**{*;}
+-keep class com.ta.utdid2.device.**{*;}
+#防止inline
+-dontoptimize
 # ============忽略警告，否则打包可能会不成功=============
 -ignorewarnings
--- app/src/main/java/demo/third/com/exceldemo/app/CustomApplication.java
+++ app/src/main/java/demo/third/com/exceldemo/app/CustomApplication.java
@@ -7,6 +7,9 @@ import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
+import com.taobao.sophix.PatchStatus;
+import com.taobao.sophix.SophixManager;
+import com.taobao.sophix.listener.PatchLoadStatusListener;
 import com.tencent.bugly.crashreport.CrashReport;
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.socialize.PlatformConfig;
@@ -57,10 +60,12 @@ public class CustomApplication extends MultiDexApplication {
         instance = this;
         activityList = new ArrayList<>();
 //        handleSSLHandshake();
-        UMConfigure.init(this, "5a12384aa40fa3551f0001d1", "umeng", UMConfigure.DEVICE_TYPE_PHONE, "");
+        UMConfigure.init(this, "5a12384aa40fa3551f0001d1", "umeng", UMConfigure
+                .DEVICE_TYPE_PHONE, "");
         PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
-        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "https://sns.whalecloud.com/sina2/callback");
+        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad",
+                "https://sns.whalecloud.com/sina2/callback");
         CrashReport.initCrashReport(getApplicationContext(), BuildConfig.BUGLYID, true);
         PreferenceHelper.init(this);
 //        PushAgent mPushAgent = PushAgent.getInstance(this);
@@ -118,6 +123,8 @@ public class CustomApplication extends MultiDexApplication {
                 .build();
 
         OkHttpUtils.initClient(client);
+        // 阿里热修复
+        SophixManager.getInstance().queryAndLoadNewPatch();
     }
 
     /**
@@ -127,6 +134,26 @@ public class CustomApplication extends MultiDexApplication {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
+        SophixManager.getInstance().setContext(this)
+                .setAppVersion("1.0")
+                .setAesKey(null)
+                .setEnableDebug(true)
+                .setPatchLoadStatusStub(new PatchLoadStatusListener() {
+                    @Override
+                    public void onLoad(final int mode, final int code, final String info, final
+                    int handlePatchVersion) {
+                        // 补丁加载回调通知
+                        if (code == PatchStatus.CODE_LOAD_SUCCESS) {
+                            // 表明补丁加载成功
+                        } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
+                            // 表明新补丁生效需要重启. 开发者可提示用户或者强制重启;
+                            // 建议: 用户可以监听进入后台事件, 然后调用killProcessSafely自杀，以此加快应用补丁，详见1.3.2.3
+                        } else {
+                            // 其它错误信息, 查看PatchStatus类说明
+                        }
+                    }
+                }).initialize();
+        // queryAndLoadNewPatch不可放在attachBaseContext 中，否则无网络权限，建议放在后面任意时刻，如onCreate中
     }
 
     @Override
