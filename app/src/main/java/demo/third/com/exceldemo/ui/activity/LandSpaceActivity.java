e3514f78cd0fca64924475002fecdf298033161c
宋正朋 <ssongzhengpeng@163.com>
2018-08-03 00:22:14 +0800

    证券公司私募投资基金

--- app/src/main/java/demo/third/com/exceldemo/ui/activity/LandSpaceActivity.java
+++ app/src/main/java/demo/third/com/exceldemo/ui/activity/LandSpaceActivity.java
@@ -1,9 +1,11 @@
 package demo.third.com.exceldemo.ui.activity;
 
-import android.graphics.Color;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -14,9 +16,7 @@ import com.zhy.http.okhttp.callback.StringCallback;
 
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import butterknife.BindView;
@@ -27,10 +27,12 @@ import demo.third.com.exceldemo.ui.adapter.LandSpaceAdapter;
 import demo.third.com.exceldemo.ui.views.MyListView;
 import demo.third.com.exceldemo.ui.views.RadioGroupEx;
 import demo.third.com.exceldemo.utils.CustomGson;
+import demo.third.com.exceldemo.utils.Tools;
 import okhttp3.Call;
 
 import static demo.third.com.exceldemo.utils.Constant.INTENT_FLAG;
 import static demo.third.com.exceldemo.utils.Link.SEARCH;
+import static demo.third.com.exceldemo.utils.Link.SEARCH_POF_SUBFUND;
 
 /**
  * @author peter
@@ -108,16 +110,40 @@ public class LandSpaceActivity extends BaseActivity implements RadioGroupEx
     RadioGroupEx rgTssx;
     @BindView(R.id.rg_cxxx)
     RadioGroupEx rgCxxx;
+    @BindView(R.id.ll_top1)
+    LinearLayout llTop1;
+    @BindView(R.id.et_pro_name)
+    EditText etProName;
+    @BindView(R.id.tv_title2)
+    TextView tvTitle2;
+    @BindView(R.id.et_manage_org)
+    EditText etManageOrg;
+    @BindView(R.id.et_pro_number)
+    EditText etProNumber;
+    @BindView(R.id.tv_time1)
+    TextView tvTime1;
+    @BindView(R.id.tv_time2)
+    TextView tvTime2;
+    @BindView(R.id.tv_search)
+    TextView tvSearch;
+    @BindView(R.id.ll_top2)
+    LinearLayout llTop2;
+    @BindView(R.id.ll_red1)
+    LinearLayout llRed1;
+    @BindView(R.id.ll_red2)
+    LinearLayout llRed2;
 
     private SearchResultEntity searchResultEntity;
     private SearchResultEntity.ResultBean resultBean;
     private LandSpaceAdapter infoAdapter;
     private String flag;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
+        progressDialog = initDialog(progressDialog, LandSpaceActivity.this, "查询中...");
         bindListener();
     }
 
@@ -128,17 +154,24 @@ public class LandSpaceActivity extends BaseActivity implements RadioGroupEx
         if (!TextUtils.isEmpty(flag)) {
             tvTitle.setText(flag);
             if (getResources().getString(R.string.tip_smjjglrflgs).equals(flag)) {
-                llCondition1.setVisibility(View.VISIBLE);
-                llCondition2.setVisibility(View.VISIBLE);
-                llCondition3.setVisibility(View.VISIBLE);
-                ckAbnormalLiquidation.setVisibility(View.VISIBLE);
-                ll_search_all.setVisibility(View.VISIBLE);
+                // 私募基金管理人分类公示
+                llTop1.setVisibility(View.VISIBLE);
+                llRed1.setVisibility(View.VISIBLE);
+                llTop2.setVisibility(View.GONE);
+                llRed2.setVisibility(View.GONE);
+                search("");
+            } else if (getResources().getString(R.string.txt_private_fund).equals(flag)) {
+                // 证券公司私募投资基金
+                llTop1.setVisibility(View.GONE);
+                llRed1.setVisibility(View.GONE);
+                llTop2.setVisibility(View.VISIBLE);
+                llRed2.setVisibility(View.VISIBLE);
+
             } else {
-                llCondition1.setVisibility(View.GONE);
-                llCondition2.setVisibility(View.GONE);
-                llCondition3.setVisibility(View.GONE);
-                ckAbnormalLiquidation.setVisibility(View.GONE);
-                ll_search_all.setVisibility(View.GONE);
+                llTop1.setVisibility(View.VISIBLE);
+                llRed1.setVisibility(View.VISIBLE);
+                llTop2.setVisibility(View.GONE);
+                llRed2.setVisibility(View.GONE);
                 search("");
             }
             switch (flag) {
@@ -235,6 +268,42 @@ public class LandSpaceActivity extends BaseActivity implements RadioGroupEx
         rgCxxx.setOnCheckedChangeListener(this);
         rgJjgm.setOnCheckedChangeListener(this);
         rgTssx.setOnCheckedChangeListener(this);
+        etProName.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent
+                        .ACTION_UP) {
+                    //业务代码
+                    readySearch();
+                    return true;
+                }
+                return false;
+            }
+        });
+        etProNumber.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent
+                        .ACTION_UP) {
+                    //业务代码
+                    readySearch();
+                    return true;
+                }
+                return false;
+            }
+        });
+        etManageOrg.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent
+                        .ACTION_UP) {
+                    //业务代码
+                    readySearch();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -263,14 +332,14 @@ public class LandSpaceActivity extends BaseActivity implements RadioGroupEx
                 searchResultEntity = CustomGson.fromJson(response, SearchResultEntity.class);
                 if (searchResultEntity != null) {
                     resultBean = searchResultEntity.getResult();
-                    infoAdapter = new LandSpaceAdapter(LandSpaceActivity.this, resultBean);
+                    infoAdapter = new LandSpaceAdapter(LandSpaceActivity.this, resultBean, flag);
                     lvPrivateFund.setAdapter(infoAdapter);
                 }
             }
         });
     }
 
-    @OnClick({R.id.iv_backup, R.id.tv_search_all})
+    @OnClick({R.id.iv_backup, R.id.tv_search_all, R.id.tv_clear_condition, R.id.tv_search})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.iv_backup:
@@ -279,11 +348,105 @@ public class LandSpaceActivity extends BaseActivity implements RadioGroupEx
             case R.id.tv_search_all:
                 search(flag);
                 break;
+            // 清空条件
+            case R.id.tv_clear_condition:
+                clearAllCondition();
+                break;
+            // 搜索
+            case R.id.tv_search:
+                readySearch();
+                break;
             default:
                 break;
         }
     }
 
+    /**
+     * 搜索前的数据采集
+     */
+    private void readySearch() {
+        // 起始时间
+        String time1 = tvTime1.getText().toString();
+        // 结束时间
+        String time2 = tvTime2.getText().toString();
+        if (!TextUtils.isEmpty(time1)) {
+            time1 = Tools.date2TimeStamp(time1);
+        }
+        if (!TextUtils.isEmpty(time2)) {
+            time2 = Tools.date2TimeStamp(time2);
+        }
+        searchZqgszgcp(etProName.getText().toString(), etProNumber.getText().toString(),
+                etManageOrg.getText().toString(), time1, time2);
+
+    }
+
+
+    private void clearAllCondition() {
+        etProName.setText("");
+        etManageOrg.setText("");
+        etProNumber.setText("");
+        tvTime1.setText("");
+        tvTime2.setText("");
+    }
+
+
+    private void searchZqgszgcp(String productName, String productCode, String mgrName, String
+            foundDateFrom, String foundDateTo) {
+        if (progressDialog != null) {
+            progressDialog.show();
+        }
+        Map<String, String> params = new HashMap<>();
+        params.put("pageIndex", "1");
+        params.put("pageSize", "50");
+        if (!TextUtils.isEmpty(productName)) {
+            params.put("productName", productName);
+        }
+        if (!TextUtils.isEmpty(productCode)) {
+            params.put("productCode", productCode);
+        }
+        if (!TextUtils.isEmpty(mgrName)) {
+            params.put("mgrName", mgrName);
+        }
+        if (!TextUtils.isEmpty(foundDateFrom)) {
+            params.put("foundDateFrom", foundDateFrom);
+        }
+        if (!TextUtils.isEmpty(foundDateTo)) {
+            params.put("foundDateTo", foundDateTo);
+        }
+
+
+        OkHttpUtils.post().url(SEARCH_POF_SUBFUND).params(params)
+                .build().execute(new StringCallback() {
+            @Override
+            public void onError(Call call, Exception e, int id) {
+                if (progressDialog != null) {
+                    progressDialog.cancel();
+                }
+            }
+
+            @Override
+            public void onResponse(String response, int id) {
+                searchResultEntity = CustomGson.fromJson(response, SearchResultEntity.class);
+                if (searchResultEntity != null) {
+                    resultBean = searchResultEntity.getResult();
+                    infoAdapter = new LandSpaceAdapter(LandSpaceActivity.this, resultBean, flag);
+                    lvPrivateFund.setAdapter(infoAdapter);
+                    try {
+                        if (resultBean.getPofSubfunds() == null || resultBean.getPofSubfunds().getList() == null
+                                || resultBean.getPofSubfunds().getList().size() == 0) {
+                            Tools.toast("暂无符合当前筛选条件的结果");
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Tools.toast("暂无符合当前筛选条件的结果");
+                    }
+                    if (progressDialog != null) {
+                        progressDialog.cancel();
+                    }
+                }
+            }
+        });
+    }
 
     @Override
     public void onCheckedChanged(RadioGroupEx group, int checkedId) {
--- app/src/main/java/demo/third/com/exceldemo/ui/adapter/LandSpaceAdapter.java
+++ app/src/main/java/demo/third/com/exceldemo/ui/adapter/LandSpaceAdapter.java
@@ -1,10 +1,12 @@
 package demo.third.com.exceldemo.ui.adapter;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -27,16 +29,33 @@ public class LandSpaceAdapter extends BaseAdapter {
     private ViewHolder holder;
     private SearchResultEntity.ResultBean resultBean;
     private List<SearchResultEntity.ResultBean.FundAccountsBean.ListBean> dataList;
+    private String flag;
 
 
-    public LandSpaceAdapter(Context context, SearchResultEntity.ResultBean resultBean) {
+    public LandSpaceAdapter(Context context, SearchResultEntity.ResultBean resultBean, String flag) {
         mContext = context;
         this.resultBean = resultBean;
+        this.flag = flag;
     }
 
     @Override
     public int getCount() {
-        return resultBean == null ? 0 : resultBean.getFundAccounts().getList().size();
+        int size = 0;
+        if (!TextUtils.isEmpty(flag)) {
+            switch (flag) {
+                case "私募基金管理人分类公示":
+                    size = resultBean == null ? 0 : resultBean.getFundAccounts().getList().size();
+                    break;
+                case "证券公司私募投资基金":
+                    size = resultBean == null ? 0 : resultBean.getPofSubfunds().getList().size();
+                    break;
+                default:
+                    size = 0;
+                    break;
+            }
+        }
+        return size;
+
     }
 
     @Override
@@ -63,13 +82,44 @@ public class LandSpaceAdapter extends BaseAdapter {
     }
 
     private void initView(int position) {
-        holder.tvNumber.setText(String.valueOf(position + 1));
-        holder.tvSmjjglrmc.setText(resultBean.getFundAccounts().getList().get(position).getName());
-        holder.tvProductsName.setText(resultBean.getFundAccounts().getList().get(position).getRegisterCode());
-        holder.tvDengjiNumber.setText(resultBean.getFundAccounts().getList().get(position).getRegisterCode());
-        holder.tvZhuceAddress.setText(Tools.timeStamp2Date(resultBean.getFundAccounts().getList().get(position).getRegisterDate() + "", ""));
-        holder.tvCreateTime.setText(Tools.timeStamp2Date(resultBean.getFundAccounts().getList().get(position).getRegisterDate() + "", ""));
-        holder.tvSignTime.setText(Tools.timeStamp2Date(resultBean.getFundAccounts().getList().get(position).getGmtCreate() + "", ""));
+        if (!TextUtils.isEmpty(flag)) {
+            switch (flag) {
+                case "私募基金管理人分类公示":
+                    holder.ll_top1.setVisibility(View.VISIBLE);
+                    holder.ll_top2.setVisibility(View.GONE);
+                    holder.tvNumber.setText(String.valueOf(position + 1));
+                    holder.tvSmjjglrmc.setText(resultBean.getFundAccounts().getList().get(position).getName());
+                    holder.tvProductsName.setText(resultBean.getFundAccounts().getList().get(position).getRegisterCode());
+                    holder.tvDengjiNumber.setText(resultBean.getFundAccounts().getList().get(position).getRegisterCode());
+                    holder.tvZhuceAddress.setText(Tools.timeStamp2Date(resultBean.getFundAccounts().getList().get(position).getRegisterDate() + "", ""));
+                    holder.tvCreateTime.setText(Tools.timeStamp2Date(resultBean.getFundAccounts().getList().get(position).getRegisterDate() + "", ""));
+                    holder.tvSignTime.setText(Tools.timeStamp2Date(resultBean.getFundAccounts().getList().get(position).getGmtCreate() + "", ""));
+                    break;
+                case "证券公司私募投资基金":
+                    holder.ll_top1.setVisibility(View.GONE);
+                    holder.ll_top2.setVisibility(View.VISIBLE);
+                    holder.tvNumber2.setText(String.valueOf(position + 1));
+                    holder.tvSmjjglrmc2.setText(resultBean.getPofSubfunds().getList().get(position).getProductCode());
+                    holder.tvProductsName2.setText(resultBean.getPofSubfunds().getList().get(position).getProductName());
+                    holder.tvDengjiNumber2.setText(resultBean.getPofSubfunds().getList().get(position).getMgrName());
+                    holder.tvZhuceAddress2.setText(resultBean.getPofSubfunds().getList().get(position).getTrustee());
+                    holder.tvCreateTime2.setText(Tools.timeStamp2Date(resultBean.getPofSubfunds().getList().get(position).getFoundDate() + "", ""));
+                    holder.tvSignTime2.setText(Tools.timeStamp2Date(resultBean.getPofSubfunds().getList().get(position).getRegisteredDate() + "", ""));
+
+                    break;
+                default:
+                    holder.ll_top1.setVisibility(View.VISIBLE);
+                    holder.ll_top2.setVisibility(View.GONE);
+                    holder.tvNumber.setText(String.valueOf(position + 1));
+                    holder.tvSmjjglrmc.setText(resultBean.getFundAccounts().getList().get(position).getName());
+                    holder.tvProductsName.setText(resultBean.getFundAccounts().getList().get(position).getRegisterCode());
+                    holder.tvDengjiNumber.setText(resultBean.getFundAccounts().getList().get(position).getRegisterCode());
+                    holder.tvZhuceAddress.setText(Tools.timeStamp2Date(resultBean.getFundAccounts().getList().get(position).getRegisterDate() + "", ""));
+                    holder.tvCreateTime.setText(Tools.timeStamp2Date(resultBean.getFundAccounts().getList().get(position).getRegisterDate() + "", ""));
+                    holder.tvSignTime.setText(Tools.timeStamp2Date(resultBean.getFundAccounts().getList().get(position).getGmtCreate() + "", ""));
+                    break;
+            }
+        }
     }
 
     static class ViewHolder {
@@ -87,6 +137,29 @@ public class LandSpaceAdapter extends BaseAdapter {
         TextView tvCreateTime;
         @BindView(R.id.tv_sign_time)
         TextView tvSignTime;
+
+        @BindView(R.id.tv_number2)
+        TextView tvNumber2;
+        @BindView(R.id.tv_smjjglrmc2)
+        TextView tvSmjjglrmc2;
+        @BindView(R.id.tv_products_name2)
+        TextView tvProductsName2;
+        @BindView(R.id.tv_zhuce_address2)
+        TextView tvZhuceAddress2;
+        @BindView(R.id.tv_dengji_number2)
+        TextView tvDengjiNumber2;
+        @BindView(R.id.tv_create_time2)
+        TextView tvCreateTime2;
+        @BindView(R.id.tv_sign_time2)
+        TextView tvSignTime2;
+
+        @BindView(R.id.ll_top1)
+        LinearLayout ll_top1;
+        @BindView(R.id.ll_top2)
+        LinearLayout ll_top2;
+
+
+
 
         ViewHolder(View view) {
             ButterKnife.bind(this, view);
